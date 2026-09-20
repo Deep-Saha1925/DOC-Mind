533d687ee48358444602d197147c92ac5ccb1663
Deep-Saha1925 <dip23447@gmail.com>
2026-09-20 21:57:06 +0530

    Updated DocumentMetaData

--- src/main/java/com/deep/docmind/entity/DocumentMetaData.java
+++ src/main/java/com/deep/docmind/entity/DocumentMetaData.java
@@ -1,4 +1,44 @@
 package com.deep.docmind.entity;
 
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+@Entity
+@Table(name = "document_metadata")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
 public class DocumentMetaData {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private UUID id;
+
+    @Column(nullable = false)
+    private String filename;
+
+    @Column(nullable = false)
+    private String contentType;
+    private Long fileSize;
+    private Integer totalPages;
+    private Integer totalChunks;
+
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false)
+    private DocumentStatus status;
+
+    @Column(length = 1000)
+    private String errorMessage;
+    @Column(nullable = false, updatable = false)
+    private LocalDateTime createdAt;
+    private LocalDateTime updatedAt;
+
+
 }
