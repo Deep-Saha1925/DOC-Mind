4406fc4d932f0a653e1c8fb5266cb24fa8e3d686
Deep-Saha1925 <dip23447@gmail.com>
2026-09-20 22:37:37 +0530

    Added GlobalExceptionHandler

---
+++ src/main/java/com/deep/docmind/exception/GlobalExceptionHandler.java
@@ -0,0 +1,30 @@
+package com.deep.docmind.exception;
+
+import com.deep.docmind.dto.ApiResponse;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.time.LocalDateTime;
+
+@RestController
+public class GlobalExceptionHandler {
+
+    private static final Logger logger = LoggerFactory.getLogger(GlobalExceptionHandler.class);
+
+    @ExceptionHandler(ResourceNotFoundException.class)
+    public ResponseEntity<?> handleNotFound(ResourceNotFoundException ex){
+        logger.warn("Resource not found : {}", ex.getMessage());
+
+        return ResponseEntity.status(HttpStatus.NOT_FOUND)
+                .body(ApiResponse.builder()
+                        .success(false)
+                        .message(ex.getMessage())
+                        .data(null)
+                        .timestamp(LocalDateTime.now())
+                        .build());
+    }
+}
