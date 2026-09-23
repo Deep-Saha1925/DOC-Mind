217321d7055b98656643dd8bef0405fea6b1e4b1
Deep-Saha1925 <dip23447@gmail.com>
2026-09-23 09:58:28 +0530

    New methods in document repo

--- src/main/java/com/deep/docmind/repository/DocumentMetadataRepo.java
+++ src/main/java/com/deep/docmind/repository/DocumentMetadataRepo.java
@@ -1,11 +1,17 @@
 package com.deep.docmind.repository;
 
 import com.deep.docmind.entity.DocumentMetaData;
+import com.deep.docmind.entity.DocumentStatus;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
 import java.util.UUID;
 
 @Repository
 public interface DocumentMetadataRepo extends JpaRepository<DocumentMetaData, UUID> {
+
+    List<DocumentMetaData> findByStatus(DocumentStatus status);
+
+    List<DocumentMetaData> findAllByOrderByCreatedAtDesc();
 }
