1871ca472d1eab090bbc07e1bf62b49a4fffbee5
Deep-Saha1925 <dip23447@gmail.com>
2026-09-23 10:05:02 +0530

    Method for finding last messages by Id

--- src/main/java/com/deep/docmind/repository/ChatMessageRepository.java
+++ src/main/java/com/deep/docmind/repository/ChatMessageRepository.java
@@ -2,6 +2,8 @@ package com.deep.docmind.repository;
 
 import com.deep.docmind.entity.ChatMessage;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
@@ -10,5 +12,9 @@ import java.util.List;
 public interface ChatMessageRepository extends JpaRepository<ChatMessage, Long> {
 
     List<ChatMessage> findByConversation_IdOrderByCreatedAtAsc(String conversationId);
+
+    //    jpql query
+    @Query(value = "SELECT * FROM chat_messages WHERE conversation_id = :conversationId ORDER BY created_at DESC LIMIT :lastN", nativeQuery = true)
+    List<ChatMessage> findLastNMessages(@Param("conversationId") String conversationId, @Param("lastN") int lastN);
 
 }
