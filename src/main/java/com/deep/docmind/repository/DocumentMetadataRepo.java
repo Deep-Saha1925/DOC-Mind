e045e7171be109b2693e76f352a6387d050bf744
Deep-Saha1925 <dip23447@gmail.com>
2026-09-23 09:59:22 +0530

    Find user and Created ar method in DocumentMetadataRepo

--- src/main/java/com/deep/docmind/repository/DocumentMetadataRepo.java
+++ src/main/java/com/deep/docmind/repository/DocumentMetadataRepo.java
@@ -2,10 +2,12 @@ package com.deep.docmind.repository;
 
 import com.deep.docmind.entity.DocumentMetaData;
 import com.deep.docmind.entity.DocumentStatus;
+import com.deep.docmind.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
+import java.util.Optional;
 import java.util.UUID;
 
 @Repository
@@ -14,4 +16,8 @@ public interface DocumentMetadataRepo extends JpaRepository<DocumentMetaData, UU
     List<DocumentMetaData> findByStatus(DocumentStatus status);
 
     List<DocumentMetaData> findAllByOrderByCreatedAtDesc();
+
+    List<DocumentMetaData> findByUserOrderByCreatedAtDesc(User user);
+
+    Optional<DocumentMetaData> findByIdAndUser(UUID id, User user);
 }
