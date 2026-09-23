5133ee1d0303dd23e26a80d02394da9706e3dde6
Deep-Saha1925 <dip23447@gmail.com>
2026-09-23 10:14:07 +0530

    Finding and deleting user method added in ConversationRepository interface

--- src/main/java/com/deep/docmind/repository/ConversationRepository.java
+++ src/main/java/com/deep/docmind/repository/ConversationRepository.java
@@ -6,9 +6,12 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
+import java.util.Optional;
 
 @Repository
 public interface ConversationRepository extends JpaRepository<Conversation, String> {
 
-    List<Conversation> findByUserOrderByUpdateAtDesc(User user);
+    List<Conversation> findByUserOrderByUpdatedAtDesc(User user);
+    Optional<Conversation> findByIdAndUser(String id, User user);
+    void deleteByIdAndUser(String id, User user);
 }
