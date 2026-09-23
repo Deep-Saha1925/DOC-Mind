9f92e3c4ed7f3dfaef1fccac548592382e53a9bb
Deep-Saha1925 <dip23447@gmail.com>
2026-09-23 10:02:44 +0530

    Listing chat messages by conversation

--- src/main/java/com/deep/docmind/repository/ChatMessageRepository.java
+++ src/main/java/com/deep/docmind/repository/ChatMessageRepository.java
@@ -2,6 +2,13 @@ package com.deep.docmind.repository;
 
 import com.deep.docmind.entity.ChatMessage;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
+@Repository
 public interface ChatMessageRepository extends JpaRepository<ChatMessage, Long> {
+
+    List<ChatMessage> findByConversation_IdOrderByCreatedAtAsc(String conversationId);
+
 }
