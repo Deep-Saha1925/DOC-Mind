a9384a85aaf6132a428a41b25a208fb2f284416f
Deep-Saha1925 <dip23447@gmail.com>
2026-09-23 10:12:06 +0530

    Find by User order method

--- src/main/java/com/deep/docmind/repository/ConversationRepository.java
+++ src/main/java/com/deep/docmind/repository/ConversationRepository.java
@@ -1,10 +1,14 @@
 package com.deep.docmind.repository;
 
 import com.deep.docmind.entity.Conversation;
+import com.deep.docmind.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 @Repository
 public interface ConversationRepository extends JpaRepository<Conversation, String> {
 
+    List<Conversation> findByUserOrderByUpdateAtDesc(User user);
 }
