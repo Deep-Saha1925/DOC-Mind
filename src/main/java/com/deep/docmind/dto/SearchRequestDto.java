75fa3c97dfe226c2056c97014de881ef75bfff76
Deep-Saha1925 <dip23447@gmail.com>
2026-09-24 10:30:24 +0530

    Dto for searching chunks

---
+++ src/main/java/com/deep/docmind/dto/SearchRequestDto.java
@@ -0,0 +1,21 @@
+package com.deep.docmind.dto;
+
+import jakarta.validation.constraints.NotBlank;
+import lombok.*;
+
+import java.util.UUID;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class SearchRequestDto {
+
+    @NotBlank(message = "Query cannot be empty")
+    private String query;
+
+    private UUID documentId;
+    private  Integer topK;
+    private  Double similaritySearch;
+}
