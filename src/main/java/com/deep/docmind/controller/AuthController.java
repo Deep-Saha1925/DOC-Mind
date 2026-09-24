4f71f9bb1504b65f1f499325718e119e55657166
Deep-Saha1925 <dip23447@gmail.com>
2026-09-24 10:04:40 +0530

    Importing all the required services

--- src/main/java/com/deep/docmind/controller/AuthController.java
+++ src/main/java/com/deep/docmind/controller/AuthController.java
@@ -1,4 +1,21 @@
 package com.deep.docmind.controller;
 
+import com.deep.docmind.repository.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/v1/auth")
+@RequiredArgsConstructor
 public class AuthController {
+
+    private final UserService userService;
+    private final AuthenticationManager authenticationManager;
+    private final UserRepository userRepository;
+    private final JwtService jwtService;
+
+
+
 }
