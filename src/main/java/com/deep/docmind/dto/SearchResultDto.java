f6ee3a3d548bbd743a5001da801724af4e640bba
Deep-Saha1925 <dip23447@gmail.com>
2026-09-24 10:31:50 +0530

    Dto for searching results of chunks

---
+++ src/main/java/com/deep/docmind/dto/SearchResultDto.java
@@ -0,0 +1,16 @@
+package com.deep.docmind.dto;
+
+import lombok.*;
+
+import java.util.List;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class SearchResultDto {
+    private String query;
+    private int totalMatches;
+    private List<CitationDto> matches;
+}
