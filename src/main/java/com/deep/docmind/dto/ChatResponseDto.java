ec2ae17a6749c114c531bed280b9c9bcb1802552
Deep-Saha1925 <dip23447@gmail.com>
2026-09-24 10:20:10 +0530

    Added ChatResponseDto

---
+++ src/main/java/com/deep/docmind/dto/ChatResponseDto.java
@@ -0,0 +1,18 @@
+package com.deep.docmind.dto;
+
+import lombok.*;
+
+import java.util.List;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class ChatResponseDto {
+
+    private String answer;
+    private String conversationId;
+    private List<CitationDto> citations;
+    private Long responseTimeMs;
+}
