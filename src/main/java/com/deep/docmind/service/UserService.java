7ab155e292b2cb75391261cceea5bcea956fc73d
Deep-Saha1925 <dip23447@gmail.com>
2026-09-25 15:02:33 +0530

    Updated User service class with utility methods

---
+++ src/main/java/com/deep/docmind/service/UserService.java
@@ -0,0 +1,37 @@
+package com.substring.docmind.service;
+
+import com.deep.docmind.dto.RegisterUserRequest;
+import com.deep.docmind.dto.UserDto;
+import com.deep.docmind.entity.Role;
+import com.deep.docmind.entity.User;
+import com.deep.docmind.repository.UserRepository;
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class UserService {
+    private final UserRepository userRepository;
+    private final PasswordEncoder passwordEncoder;
+
+    public UserDto registerUser(@Valid RegisterUserRequest registerUserRequest) {
+
+        //validation
+        if(registerUserRequest.username().isBlank()){
+            throw new IllegalArgumentException("Username cannot be blank");
+        }
+        if(userRepository.existsByUsername(registerUserRequest.username())){
+            throw new IllegalArgumentException("Username already exists");
+        }
+
+        User user = new User();
+        user.setUsername(registerUserRequest.username());
+        user.setEmail(registerUserRequest.email());
+        user.setPassword(passwordEncoder.encode(registerUserRequest.password()));
+        user.setRole(Role.USER);
+        User savedUser = userRepository.save(user);
+        return new UserDto(savedUser.getId(), savedUser.getUsername(), savedUser.getEmail(), savedUser.getRole());
+    }
+}
