e146278fd485cabc73620828f6bf497f70e7922d
Deep-Saha1925 <dip23447@gmail.com>
2026-09-26 20:19:41 +0530

    Requestmapping endpoint in Chat Controller

--- src/main/java/com/deep/docmind/controller/ChatController.java
+++ src/main/java/com/deep/docmind/controller/ChatController.java
@@ -1,12 +1,21 @@
 package com.deep.docmind.controller;
 
+import io.swagger.v3.oas.annotations.tags.Tag;
+import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/api/v1/chats")
+@RequestMapping("/api/v1/chat")
+@Tag(
+        name = "Chat Management",
+        description = "All chat related apis goes here."
+)
+@RequiredArgsConstructor
 public class ChatController {
+
+
 
     public ResponseEntity<String> chat(){
         return ResponseEntity.ok("CHATTING");
